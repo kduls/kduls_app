d295a990329637b3c4a3b9e9b7beeffe79dbeaa3
kduls <kdulsgm@gmail.com>
2019-07-06 13:11:16 +0300

    Fixes & Flash Messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_articles_show(@article)
+		if @article.save
+			flash[:notice] = "Creation of Article Succesfull"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+		
+	end
+
+	def show
+		@article = Article.find(params[:id])
 	end
 
     private
